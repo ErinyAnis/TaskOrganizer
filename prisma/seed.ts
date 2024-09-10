a55cd93880b492c7c3c04bc5b6b0ba308cc1929a
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2024-09-10 16:07:30 +0300

    fix seed error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,18 @@
 import { PrismaClient } from "@prisma/client";
-import { faker } from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
 async function main() {
   //** generate fake data for Todo model */
-  await prisma.todo.createMany({
-    data: Array.from({ length: 25 }, () => ({
-      title: faker.lorem.words({ min: 2, max: 5 }),
-      body: faker.lorem.words({ min: 1, max: 10 }),
-    })),
-  });
+  // await prisma.todo.createMany({
+  //   data: Array.from({ length: 25 }, () => ({
+  //     title: faker.lorem.words({ min: 2, max: 5 }),
+  //     body: faker.lorem.words({ min: 1, max: 10 }),
+  //     //todo replace the id below with your own id after logging
+  //     user_id: "user_2lsHWDbDq0wsnAvqFUks2n2VZxY"
+  //   })),
+  // });
 
   //** generate fake data for user model */
   // await prisma.user.createMany({
